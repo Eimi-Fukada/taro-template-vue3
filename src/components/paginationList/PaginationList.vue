db90839cf0af4b1bb8787b693408ef00c85ef945
grace <1745282879@qq.com>
2025-08-25 15:09:19 +0800

    feat: taro不支持svg标签，H5上能展示，小程序展示不了，使用单标签实现

--- src/components/paginationList/PaginationList.vue
+++ src/components/paginationList/PaginationList.vue
@@ -7,7 +7,8 @@
       :style="{ height: pullRefreshHeight + 'px' }"
     >
       <view :class="styles.pullRefreshContent">
-        <svg class="animate-spin h-5 w-5" viewBox="0 0 24 24" fill="none">
+        <view :class="styles.spinner" />
+        <!-- <svg class="animate-spin h-5 w-5" viewBox="0 0 24 24" fill="none">
           <circle
             cx="12"
             cy="12"
@@ -21,7 +22,7 @@
             d="m4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             class="opacity-75"
           />
-        </svg>
+        </svg> -->
       </view>
     </view>
 
