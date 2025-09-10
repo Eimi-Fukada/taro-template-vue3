411ca1e6e25b0d947f70930fb82b9cdfe6cdb1b6
grace <1745282879@qq.com>
2025-09-10 14:56:02 +0800

    feat: 添加微信小程序的上传

--- src/components/upload/index.ts
+++ src/components/upload/index.ts
@@ -32,3 +32,36 @@ export async function uploadAliyun(fileList: File[]) {
 
   return Promise.all(uploadTasks)
 }
+
+/**
+ *
+ * @param fileList
+ * @returns
+ * 微信小程序环境中没有 FormData 对象。FormData 是浏览器 Web API 的一部分
+ */
+export async function uploadMiniAliyun(fileList: string[]) {
+  const uploadTasks = fileList.map((file) => {
+    return Taro.uploadFile({
+      url: apiUrl + '/resource/oss/upload',
+      filePath: file,
+      name: 'file',
+      /** HTTP 请求中其他额外的 form data */
+      // formData: {
+      //   user: 'test',
+      // },
+      header: {
+        Authorization: 'Bearer ' + Taro.getStorageSync('Authorization'),
+        clientId: '428a8310cd442757ae699df5d894f052',
+      },
+      success: (res) => {
+        const data = res.data
+        return data
+      },
+      fail: (err) => {
+        return Promise.reject(err)
+      },
+    })
+  })
+
+  return Promise.all(uploadTasks)
+}
