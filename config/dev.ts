7325b21c6896813864d7d5575fd067ef19c6aa9e
grace <1745282879@qq.com>
2025-11-18 09:09:52 +0800

    feat: 添加 proxy 代理

--- config/dev.ts
+++ config/dev.ts
@@ -1,9 +1,24 @@
 module.exports = {
   env: {
-    NODE_ENV: '"development"'
+    NODE_ENV: '"development"',
   },
-  defineConstants: {
-  },
+  defineConstants: {},
   mini: {},
-  h5: {}
+  h5: {
+    // devServer: {
+    //   proxy: {
+    //     '/web': {
+    //       target: 'http://cusfront.sky.com:11180/',
+    //       pathRewrite: {
+    //         '^/web/': '',
+    //       },
+    //       changeOrigin: true,
+    //       onProxyRes(proxyRes, req, res) {
+    //         proxyRes.headers['x-real-url2'] =
+    //           `http://cusfront.sky.com:11180/${req.url}`
+    //       },
+    //     },
+    //   },
+    // },
+  },
 }
