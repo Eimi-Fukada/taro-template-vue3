411f44b96849bd1d10c19021f01c144c85396be6
grace <1745282879@qq.com>
2025-11-27 13:42:38 +0800

    feat: 开启代码热重载，开发工具那个没用

--- config/index.ts
+++ config/index.ts
@@ -46,6 +46,7 @@ const config = {
     // enable: true, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   mini: {
+    hot: true,
     webpackChain(chain, webpack) {
       chain.merge({
         module: {
